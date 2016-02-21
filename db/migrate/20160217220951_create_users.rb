c24ed09c5ce9253d4813646bddda3f3b54f02ce4
uchennafokoye <okoyefaithful@gmail.com>
2016-02-21 15:48:06 -0600

    Trying to fix heroku bug

--- db/migrate/20160217220951_create_users.rb
+++ db/migrate/20160217220951_create_users.rb
@@ -2,7 +2,6 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :token
-      t.string :email_address
       t.timestamps null: false
     end
   end
