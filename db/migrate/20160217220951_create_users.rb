6df35b4d9abc908b7dff858b6d4d03fd52cb6dd9
uchennafokoye <okoyefaithful@gmail.com>
2016-02-21 15:49:14 -0600

    Added email_address

--- db/migrate/20160217220951_create_users.rb
+++ db/migrate/20160217220951_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :token
+      t.string :email_address
       t.timestamps null: false
     end
   end
