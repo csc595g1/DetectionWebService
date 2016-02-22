aab10f1b9883a7b3edf2c998139d3025b481e680
uchennafokoye <okoyefaithful@gmail.com>
2016-02-21 21:19:14 -0600

    Moved apipie to all in gemfile

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,13 +1,8 @@
-require 'byebug'
 Apipie.configure do |config|
   config.app_name                = "Jarvis Detection API"
   config.default_version = "1"
   config.api_base_url            = ""
   config.doc_base_url            = "/api"
-  config.app_info["1.0"] = "
-    This is where you can inform user about your application and API
-    in general.
-  "
   # were is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
 end
